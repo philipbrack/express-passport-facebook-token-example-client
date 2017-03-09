1a127cb076440037696091b5d374f2a917be26a3
Philip Brack <philip.brack@gmail.com>
2017-03-08 19:01:47 -0800

    update to read.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Headers, RequestOptions, Http} from '@angular/http';
 
 import {FacebookService, FacebookLoginResponse, FacebookLoginOptions, FacebookUiParams} from 'ng2-facebook-sdk';
-import {FacebookUiResponse} from "ng2-facebook-sdk/dist/ng2-facebook-sdk";
+
+
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +15,14 @@ import {FacebookUiResponse} from "ng2-facebook-sdk/dist/ng2-facebook-sdk";
 export class AppComponent {
 
   title = 'Helloworld';
+  token = '';
 
-  constructor(
-    private fb: FacebookService
-  ) {
+  constructor(private fb: FacebookService, private http: Http) {
 
     console.log('Initializing Facebook');
 
     fb.init({
-      appId: '1927971220769787',
+      appId: '<TODO>',
       version: 'v2.8'
     });
 
@@ -31,72 +35,59 @@ export class AppComponent {
     this.fb.login()
       .then((res: FacebookLoginResponse) => {
         console.log('Logged in', res);
-      })
-      .catch(this.handleError);
-  }
-
-  /**
-   * Login with additional permissions/options
-   */
-  loginWithOptions() {
-
-    const loginOptions: FacebookLoginOptions = {
-      enable_profile_selector: true,
-      return_scopes: true,
-      scope: 'public_profile,user_friends,email,pages_show_list'
-    };
-
-    this.fb.login(loginOptions)
-      .then((res: FacebookLoginResponse) => {
-        console.log('Logged in', res);
-      })
-      .catch(this.handleError);
-
-  }
-
-
-  /**
-   * Get the user's profile
-   */
-  getProfile() {
-    this.fb.api('/me')
-      .then((res: any) => {
-        console.log('Got the users profile', res);
+        this.token = res.authResponse.accessToken;
       })
       .catch(this.handleError);
   }
 
 
-  /**
-   * Get the users friends
-   */
-  getFriends() {
-    this.fb.api('/me/friends')
-      .then((res: any) => {
-        console.log('Got the users friends', res);
+  getProtected() {
+
+    let header: Headers = new Headers();
+    console.log('token is ', this.token);
+    header.append('Content-Type', 'application/json');
+    header.append('Authorization', 'Bearer ' + this.token);
+    let options = new RequestOptions({headers: header});
+
+    this.http.get('http://localhost:3000/protected', options)
+      .toPromise()
+      .then((res) => {
+        console.log('response from server', res.status);
+        if (res.status == 200) {
+          console.log('read ', res.json());
+          return res.json();
+        } else if (res.status == 401) {
+          return null;
+        } else {
+          throw new Error('This request has failed ' + res.status);
+        }
       })
-      .catch(this.handleError);
+      .catch(err => console.error('got error', err));
   }
 
+  getProtectedWrongToken() {
 
-  /**
-   * Show the share dialog
-   */
-  share() {
+    let wrongToken = this.token.substr(0, this.token.length - 2) + 'f';
+    let header: Headers = new Headers();
+    header.append('Content-Type', 'application/json');
+    header.append('Authorization', 'Bearer ' + wrongToken);
+    let options = new RequestOptions({headers: header});
 
-    const options: FacebookUiParams = {
-      method: 'share',
-      href: 'https://github.com/zyramedia/ng2-facebook-sdk'
-    };
-
-    this.fb.ui(options)
-      .then((res: FacebookUiResponse) => {
-        console.log('Got the users profile', res);
+    this.http.get('http://localhost:3000/protected', options)
+      .toPromise()
+      .then((res) => {
+        console.log('response from server', res.status);
+        if (res.status == 200) {
+          console.log('read ', res.json());
+          return res.json();
+        } else if (res.status == 401) {
+          return null;
+        } else {
+          throw new Error('This request has failed ' + res.status);
+        }
       })
-      .catch(this.handleError);
-
+      .catch(err => console.error('got error', err));
   }
-
 
   /**
    * This is a convenience method for the sake of this example project.
